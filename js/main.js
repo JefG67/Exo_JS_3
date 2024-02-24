c1d4e029da3d95aca5171526a943ef40d1e2cbbb
JF.G <156801075+JefG67@users.noreply.github.com>
2024-02-24 19:38:22 +0100

    j'ai juste cree les  4 cubes pour le moment

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// On récupère l'élément #board
+const board = document.querySelector("#board");
+
+// On crée une box avec la class "carree"
+const box = document.createElement("div")
+box.classList.add("carree")
+
+for (let i = 1; i <= 4; i++) {
+    const newBox = box.cloneNode(); // Clone des divs
+    newBox.textContent = i; // Numéro écrit dans le bloc
+    board.appendChild(newBox);
+}
